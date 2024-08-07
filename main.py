4b188b3b7a0ecc8d3f40a130a3e903b39a190d8d
Kanayo Justice Kenechukwu <theken.dev@gmail.com>
2024-08-07 12:56:22 +0100

    model training added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from KD_Learn import logger
 from KD_Learn.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from KD_Learn.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from KD_Learn.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -21,6 +21,19 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
